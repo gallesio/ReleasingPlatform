554f33120a5e064940c4211c8a07f45f0e2e07c8
Claire Gallesio <claire.gallesio@gmail.com>
2014-04-16 10:10:28 +0200

    Added content on RM REST Service and added services (master and elementary) on flow.

--- releasingplatformmule/src/main/java/services/RM.java
+++ releasingplatformmule/src/main/java/services/RM.java
@@ -1,5 +1,33 @@
 package services;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+
+@Path("/RM")
 public class RM {
+	
+	String result = "Processing RM ...";
+	
+	public RM() {}
+	
+	@POST
+	public String instanciateProcess(String input) {
+		
+		Client client = Client.create();
+        client.addFilter(new HTTPBasicAuthFilter("kermit", "kermit"));
+        WebResource webResource = client.resource("http://localhost:8080/activiti-rest/service/process-instance");
+        
+        System.out.println(result);
+
+        ClientResponse response = webResource.post(ClientResponse.class, input);
+        String output = response.getEntity(String.class);
+        
+        return output;
+	}
 
 }
